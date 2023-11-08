8b2ef20f41e2d441e5aa6b0e5d529712c56eafab
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-08 20:12:15 +0600

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -5,6 +5,6 @@ import java.util.Date;
 public class StudentInfo
   { public static void main (String[]args){
       System.out.println("Alexander Martin");
-      System.out.println(new Date());
+      System.out.println("08.11.2023");
   }
 }
