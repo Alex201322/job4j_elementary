f11722230860971106acc6e419d88ace633da2bc
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-08 19:47:48 +0600

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.util.Date;
+
+public class StudentInfo
+  { public static void main (String[]args){
+      System.out.println("Alexander Martin");
+      System.out.println(new Date());
+  }
+}
