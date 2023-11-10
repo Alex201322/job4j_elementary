f85fd504d86e01e9c869b33d9240a0ed2708e867
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-10 22:47:27 +0600

    4.3. Вызов метода без указания класса [#504945]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,29 +1,21 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void plus(int width, int length) {
-        int square = width * length;
-        System.out.println(square);
+        public static void plus(int first, int second) {
+            int result = first + second;
+            System.out.println(result);
+        }
+
+        public static void minus(int first, int second) {
+            int result = first - second;
+            System.out.println(result);
+        }
+
+        public static void main(String[] args) {
+            plus(1, 2);
+            plus(10, 11);
+            minus(7, 3);
+            minus(2, 6);
+        }
+
     }
-
-    public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
-
-        System.out.println(sixDivTwo);
-        System.out.println(onePlusTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
-    }
-
-}
