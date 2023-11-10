ddb86edf66de83a04e62333dc5f41c573593e16f
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-10 16:34:35 +0600

    4. Методы [#231984] copy

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -7,10 +7,23 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
         Calculator.plus(100, 500);
         Calculator.plus(4, 2);
         Calculator.plus(3, 5);
 
+        System.out.println(sixDivTwo);
+        System.out.println(onePlusTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
     }
 
 }
