3bdbb9fde45ce57602993e0371939f891af4386a
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-13 01:04:16 +0600

    9.3. Математические операторы и составные операторы присваивания [#504905]

---
+++ src/main/java/ru/job4j/condition/OperatorsRunner.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+public class OperatorsRunner {
+    public static void main(String[] args) {
+        int first = 2;
+        int second = 10;
+        int result = first + second;
+        System.out.println("Result sum: " + result);
+        result = second - first;
+        System.out.println("Result subtract: " + result);
+        result = first * second;
+        System.out.println("Result multiply: " + result);
+        result = second / first;
+        System.out.println("Result divide: " + result);
+        first += second;
+        System.out.println("Result sum: " + first);
+        first = 2;
+        second -= first;
+        System.out.println("Result subtract: " + second);
+        second = 10;
+        first *= second;
+        System.out.println("Result multiply: " + first);
+        first = 2;
+        second /= first;
+        System.out.println("Result divide: " + second);
+    }
+}
