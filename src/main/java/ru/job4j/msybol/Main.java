6a8a8b36de2c057fd4fe555a481afa60a404ba3e
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-13 10:41:01 +0600

    9.4. Символы в Java и их особенности [#504920] №2

---
+++ src/main/java/ru/job4j/msybol/Main.java
@@ -0,0 +1,11 @@
+package ru.job4j.msybol;
+
+public class Main {
+    public static void main(String[] args) {
+        char d = 'D';
+        System.out.println(--d);
+
+    char a = 'A';
+        System.out.println(++a);
+    }
+}
