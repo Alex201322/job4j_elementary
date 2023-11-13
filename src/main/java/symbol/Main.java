ab5a5ad63d4eb4793812197ef2830e34b5aeae4f
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-13 10:33:28 +0600

    9.4. Символы в Java и их особенности [#504920]

---
+++ src/main/java/symbol/Main.java
@@ -0,0 +1,11 @@
+package symbol;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 'A';
+        int b = 'B';
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+        System.out.println("A + B: " + (a + b));
+    }
+}
