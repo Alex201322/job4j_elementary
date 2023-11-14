1d9cd2851abd05f5bbfdf6be8143bfa35bc809b1
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-14 16:51:21 +0600

    0. Что такое тестирование [#159431] №2

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,26 +1,32 @@
 package ru.job4j.converter;
 
 public class Converter {
+    static double kursEuro = 500.10;
+    static double kursDollar = 458.23;
+
     public static double tengeToEuro(double value) {
-         return value / 499.05;
+        return value / kursEuro;
     }
 
     public static double tengeToDollar(double value) {
-         return  value / 468.10;
+         return  value / kursDollar;
     }
 
     public static void main(String[] args) {
-        double e = 40000.05;
-        double d = 9000000000000.99;
+        double e = 2000;
+        double d = 2000;
         double euro = Converter.tengeToEuro(e);
         double dollar = Converter.tengeToDollar(d);
-        double sum1 = Math.floor(Math.round(euro * 499.05 * 100)) / 100;
-        double sum2 = Math.floor(Math.round(dollar * 468.10 * 100)) / 100;
+        double sum1 = Math.floor(Math.round(euro * kursEuro * 100)) / 100;
+        double sum2 = Math.floor(Math.round(dollar * kursDollar * 100)) / 100;
         System.out.println(sum1 + "  " + e);
         System.out.println(sum2 + "  " + d);
         boolean passed1 = sum1 == e;
         boolean passed2 = sum2 == d;
-        System.out.printf("Tenge: %.2f euro: %.2f test: %s \n", sum1, euro,  passed1);
-        System.out.printf("Tenge: %.2f dollar: %.2f test: %s \n", sum2, dollar, passed2);
+        System.out.printf("Tenge: %.0f euro: %.3f test: %s \n", sum1, euro,  passed1);
+        System.out.printf("Tenge: %.0f dollar: %.3f test: %s \n", sum2, dollar, passed2);
+
+        }
     }
-}
+
+
