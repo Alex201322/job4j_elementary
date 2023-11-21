570793886f723f23ea4bf1cfeb424eaf685495c1
Alex <101092305+Alex201322@users.noreply.github.com>
2023-11-21 16:28:43 +0600

    4.6.2. Switch - matcher (MultipleSwitchWeek)

---
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class MultipleSwitchWeek {
+    public static int numberOfDay(String name) {
+        return switch (name) {
+            case  "Monday" -> 1;
+            case "Tuesday" -> 2;
+            case "Wednesday" -> 3;
+            case "Thursday" -> 4;
+            case "Friday" -> 5;
+            case "Saturday" -> 6;
+            case "Sunday" -> 7;
+            default -> -1;
+        };
+    }
+
+    public static void main(String[] args) {
+        String name = "fghfdhdf";
+        int t = MultipleSwitchWeek.numberOfDay(name);
+        System.out.println(t);
+    }
+
+}
