35c0597f9bfcf97ed21a4a40be7d0d69e8e355e5
Alex <101092305+Alex201322@users.noreply.github.com>
2023-12-09 17:30:13 +0600

    33. Найти количество четных цифр в трехзначном числе

--- src/main/java/ru/job4j/io/Task32.java
+++ src/main/java/ru/job4j/io/Task32.java
@@ -1,29 +1,27 @@
 package ru.job4j.io;
 
 public class Task32 {
-    public static int evenAmount(int number) {
-        int num1, counter1 = 0;
-        num1 = number / 10;
-        if (num1 % 2 == 0) {
-            counter1++;
-        }
-        return counter1;
-    }
 
-    public static int evenAmount2(int number) {
-        int  num2, counter = 0;
-        num2 = number % 10;
+    public static int evenAmount(int number) {
+        int  num1, num2, num3, counter = 0;
+        num1 = number / 100;
+        num2 = number / 10 % 10;
+        num3 = number % 10;
+        if (num1 % 2 == 0) {
+            counter++;
+        }
         if (num2 % 2 == 0) {
-             counter++;
+            counter++;
+        }
+        if (num3 % 2 == 0) {
+            counter++;
         }
         return counter;
     }
 
     public static void main(String[] args) {
-        int number = 66;
-        int result1 = Task32.evenAmount(number);
-        int result2 = Task32.evenAmount2(number);
-        System.out.println(result1 + "" + result2);
-
+        int number = 624;
+        int counter = Task32.evenAmount(number);
+        System.out.println(counter);
     }
 }
